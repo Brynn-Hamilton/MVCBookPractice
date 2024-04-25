db2e38de6f441f4913e361bb0eac015b9bccc07c
Brynn Hamilton <brynn.s.hamilton@gmail.com>
2024-04-24 20:31:42 -0400

    Add project files.

---
+++ MVCBookPractice_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCBookPractice_Model.Models;
+
+namespace MVCBookPractice_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=GHOST\\SQLEXPRESS;Database=BookPractice;TrustServerCertificate=True;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().HasData(
+                new Book { Id = 1, Title = "Pawn of Prophecy", ISBN = "123B4HC", Price = 15.99m },
+                new Book { Id = 2, Title = "A Court of Thorns and Roses", ISBN = "7734JT1", Price = 35.99m }
+            );
+
+            var bookList = new Book[]
+            {
+                new Book { Id = 3, Title = "Bonded by Thorns", ISBN = "713GJK12", Price = 4.99m },
+                new Book { Id = 4, Title = "Remarried Empress", ISBN = "123IYM", Price = 1.99m },
+                new Book { Id = 5, Title = "Wilde Fae: Lord of Nightmare", ISBN = "947HD99", Price = 13.99m },
+                new Book { Id = 6, Title = "His Darkest Craving", ISBN = "341HNB4", Price = 4.99m }
+            };
+
+            modelBuilder.Entity<Book>().HasData(bookList);
+        }
+    }
+}
---
+++ MVCBookPractice_DataAccess/Migrations/20231009234749_SeedBookTable.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MVCBookPractice_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedBookTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "ISBN", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, "123B4HC", 15.99m, "Pawn of Prophecy" },
+                    { 2, "7734JT1", 35.99m, "A Court of Thorns and Roses" },
+                    { 3, "713GJK12", 4.99m, "Bonded by Thorns" },
+                    { 4, "123IYM", 1.99m, "Remarried Empress" },
+                    { 5, "947HD99", 13.99m, "Wilde Fae: Lord of Nightmare" },
+                    { 6, "341HNB4", 4.99m, "His Darkest Craving" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 6);
+        }
+    }
+}
